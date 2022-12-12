c85f0e06bf848c55dd6cd9a2dbf675bab88fe172
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-12 17:56:21 +0100

    refactor: modify css files to scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { FullCalendarApp } from "./FullCalendarApp";
-import "./styles.css";
+import "./styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
